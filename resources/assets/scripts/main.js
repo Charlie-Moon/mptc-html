e46521a3643018e4f74f9dd33f492a717a3a345c
Charlie-Moon <chanthailyios@gmail.com>
2021-01-18 13:43:00 +0700

    done index, category, detail, table

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -1,3 +1,26 @@
 import 'bootstrap';
 import './particles.min.js';
 import './particlesjs-config';
+import 'slick-carousel/slick/slick.min';
+import 'modal-video/js/jquery-modal-video.min';
+// $(document).ready( (function()) => {
+//   $('.slide-news').slick({
+//     prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+//             nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+//         });
+//   });
+// })
+
+
+$(document).ready(function(){
+  $('.slide-news, .slide-related').slick({
+      prevArrow:"<button type='button' class='slick-prev pull-left'></button>",
+      nextArrow:"<button type='button' class='slick-next pull-right'></button>"
+  });
+
+  $(".popup-video").modalVideo({
+    controls: 0,
+  });
+});
+
+import './menu';
---
+++ resources/assets/scripts/menu.js
@@ -0,0 +1,25 @@
+
+// action toggle menu
+$(document).ready(function(){
+    $(".navbar-toggler").click(function(){
+      $("body").toggleClass("nav-open nav-close");
+    });
+});
+$(document).ready(function(){
+    $(".bg-overlay, .close").click(function(){
+      $("body").removeClass("nav-open");
+    });
+});
+
+
+// toggle button search
+$(document).ready(function(){
+  $("#search").click(function(){
+    $("body").toggleClass("search-open");
+  });
+});
+$(document).ready(function(){
+  $(".close").click(function(){
+    $("body").removeClass("search-open");
+  });
+});
